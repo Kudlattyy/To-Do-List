ffb69aa1f06a780ef303869943b42217756b00e6
Kudłaty <hubert.suchorowski1@gmail.com>
2023-07-13 19:48:24 +0200

    Add simple logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '$lib/styles/app.scss';
+</script>
+
+<section>
+	<div>
+		<h1>To Do List</h1>
+	</div>
+</section>
+
+<style lang="scss">
+	h1 {
+		font-size: 5rem;
+		font-family: 'Anton', sans-serif;
+		-webkit-text-stroke: 1px #435fff;
+		text-transform: uppercase;
+		animation: test 1s;
+		margin: 0;
+	}
+
+	.Test {
+		display: flex;
+		justify-content: center;
+		justify-content: space-around;
+		div {
+			padding: 10px;
+			border: solid 2px #fff;
+			background-color: grey;
+		}
+	}
+
+	section {
+		display: flex;
+		justify-content: center;
+	}
+
+	@keyframes test {
+		from {
+			transform: translateY(400px);
+		}
+		to {
+			transform: translateY(0vh);
+		}
+	}
+</style>
